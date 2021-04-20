c58d066036059b212668b6660643b35d36d3173e
444978264 <444978264@qq.com>
2021-04-20 16:37:02 +0800

    添加项目 配置

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    printWidth: 100, // 换行字符串阈值
+    tabWidth: 4,
+    semi: true, // 句末加分号
+    singleQuote: true, // 用单引号
+    trailingComma: 'all', // 最后一个对象元素加逗号
+    bracketSpacing: false, // 对象，数组加空格 { foo: bar } or {foo: bar}
+    jsxBracketSameLine: true, // jsx > 是否另起一行
+    arrowParens: 'avoid', // (x) => {} 是否要有小括号
+    requirePragma: false, // 是否要注释来决定是否格式化代码
+    proseWrap: 'always', // 是否要换行
+    // proseWrap: 'preserve' // 是否要换行
+};
--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,15 @@
-import typescript from '@rollup/plugin-typescript';  // 让 rollup 认识 ts 的代码
-import pkg from './package.json';
+import typescript from '@rollup/plugin-typescript'; // 让 rollup 认识 ts 的代码
 
 // 为了将引入的 npm 包，也打包进最终结果中
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import pkg from './package.json';
 
 // 一段自定义的内容，以下内容会添加到打包结果中
 const footer = `
 if(typeof window !== 'undefined') {
   window._Dry_VERSION_ = '${pkg.version}'
-}`
+}`;
 
 export default {
     input: './packages/index.ts',
@@ -31,9 +31,5 @@ export default {
             footer,
         },
     ],
-    plugins: [
-        typescript(),
-        commonjs(),
-        resolve()
-    ]
-}
+    plugins: [typescript(), commonjs(), resolve()],
+};
