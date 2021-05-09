ae447e7d16b129fafdcd62581f09f98f58fd146f
debugger007 <444978264@qq.com>
2021-05-09 10:57:30 +0800

    add some rollup  plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,16 @@
-import typescript from '@rollup/plugin-typescript'; // 让 rollup 认识 ts 的代码
-
-// 为了将引入的 npm 包，也打包进最终结果中
+import typescript from '@rollup/plugin-typescript';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import json from 'rollup-plugin-json';
+import {terser} from 'rollup-plugin-terser';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 
-// 一段自定义的内容，以下内容会添加到打包结果中
 const footer = `
 if(typeof window !== 'undefined') {
-  window._Dry_VERSION_ = '${pkg.version}'
+  window._CHANNEL_SERVICE_VERSION_ = '${pkg.version}'
 }`;
 
 export default {
@@ -18,18 +20,35 @@ export default {
             file: pkg.main,
             format: 'cjs',
             footer,
+            sourcemap: true,
         },
         {
             file: pkg.module,
             format: 'esm',
             footer,
+            sourcemap: true,
         },
         {
             file: pkg.browser,
             format: 'umd',
-            name: 'Dry',
+            name: 'channelService',
+            sourcemap: true,
             footer,
         },
     ],
-    plugins: [typescript(), commonjs(), resolve()],
+    plugins: [
+        json(),
+        typescript(),
+        commonjs(),
+        resolve(),
+        terser(),
+        babel({
+            exclude: 'node_modules/**',
+        }),
+        livereload(),
+        serve({
+            open: true,
+            contentBase: 'dist',
+        }),
+    ],
 };
--- utils/event.ts
+++ utils/event.ts
@@ -4,3 +4,5 @@ export interface Event {
 }
 
 namespace Event {}
+
+class Emitter {}
