bc11dab0fbb50845c79d4ddee93d9a53b646889f
debugger007 <444978264@qq.com>
2021-05-09 22:16:26 +0800

    fix(event.ts): 修正Event.snapshot 方法

--- utils/event.ts
+++ utils/event.ts
@@ -6,8 +6,114 @@ export type Event<T = any> = (
     thisArgs?: any,
     disposables?: any,
 ) => IDispose;
+export interface ISender<T> {
+    addEventListener(channel: string, listener: (e: T) => void): void;
+    removeEventListener(channel: string, dispose: (e: T) => void): void;
+}
 export namespace Event {
-    export function test(): void {
-        console.log('test');
+    export function from<T>(
+        sender: ISender<T>,
+        channel: string,
+        map: (...args: any[]) => T,
+    ): Event<T> {
+        const listenFn = (...args: any[]) => emitter.fire(map(...args));
+        const emitter = new Emitter<T>({
+            onFirstAdd() {
+                sender.addEventListener(channel, listenFn);
+            },
+            onLastRemove() {
+                sender.removeEventListener(channel, listenFn);
+            },
+        });
+        return emitter.event;
+    }
+
+    export function filter<T>(
+        event: Event<T>,
+        callback: (e: T) => boolean,
+    ): Event<T> {
+        return snapshot(
+            (listener: (d: T) => IDispose, thisArgs = null, disposables?) => {
+                return event(
+                    function (e) {
+                        callback(e) && listener(e);
+                    },
+                    thisArgs,
+                    disposables,
+                );
+            },
+        );
+    }
+
+    export function snapshot<T>(event: Event<T>): Event<T> {
+        let listener: IDispose;
+        const emitter = new Emitter<T>({
+            onFirstAdd() {
+                listener = event(emitter.fire, emitter);
+            },
+            onLastRemove() {
+                listener.dispose();
+            },
+        });
+        return emitter.event;
+    }
+}
+
+type IListener<T = any> = [(d: T) => void, any];
+interface IEmitter {
+    onFirstAdd?(): void;
+    onLastRemove?(): void;
+}
+export class Emitter<T> implements IDispose {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    private static readonly _noop = function () {};
+    private _deliverQueue = new Set<IListener>();
+    private _event: Event<T> | null = null;
+    private _disposed = false;
+    constructor(private options: IEmitter = {}) {}
+    get event(): Event<T> {
+        if (this._event === null) {
+            this._event = function (
+                listener: (e: T) => any,
+                thisArgs: any = undefined,
+                disposables?: any,
+            ) {
+                if (!this._deliverQueue.size) {
+                    this.options.onFirstAdd?.();
+                }
+
+                const params: IListener = [listener, thisArgs];
+                this._deliverQueue.add(params);
+
+                const result = {
+                    dispose: () => {
+                        if (this._disposed) return;
+                        result.dispose = Emitter._noop;
+                        this._deliverQueue.delete(params);
+                        if (!this._deliverQueue.size) {
+                            this.options.onLastRemove?.();
+                        }
+                    },
+                };
+                return result;
+            };
+        }
+        return this._event;
+    }
+    public fire(event?: T): void {
+        if (this._deliverQueue.size > 0) {
+            this._deliverQueue.forEach(listener => {
+                const [callback, thisObj] = listener;
+                callback.call(thisObj, event);
+            });
+        }
+    }
+
+    public dispose(): void {
+        if (!this._disposed) {
+            this._disposed = true;
+            this._deliverQueue.clear();
+            this.options.onLastRemove?.();
+        }
     }
 }
