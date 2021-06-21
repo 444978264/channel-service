b99cf0604416ade2a6330acba5815e2c630cce15
debugger007 <yi444978264@gmail.com>
2021-06-21 11:40:33 +0800

    fix: 捕获拦截错误后 ，在广播出去

--- packages/Socket/WebSocket.ts
+++ packages/Socket/WebSocket.ts
@@ -120,6 +120,35 @@ export class SocketCore {
         this.hooks.emit(SOCKET_STATUS.connect);
     };
 
+    private _emitInterceptorError(reason: string, data: any) {
+        this._catchError(
+            () => {
+                throw new InterceptorError(reason, data);
+            },
+            err => {
+                this.hooks.emit(SOCKET_STATUS.interceptorError, err);
+            },
+        );
+    }
+
+    public send(params: any) {
+        if (!this._socket) {
+            this.connect();
+        }
+        this._onReady(() => {
+            const context = this._context({...params});
+            this.interceptors.request.start(
+                context,
+                () => {
+                    this._socket?.send(context.data);
+                },
+                (ctx, reason) => {
+                    this._emitInterceptorError(reason, ctx.data);
+                },
+            );
+        });
+    }
+
     private _message = (e: MessageEvent) => {
         const context = this._context(e.data);
         this.interceptors.response.start(
@@ -128,10 +157,7 @@ export class SocketCore {
                 this.hooks.emit(SOCKET_STATUS.message, context.data);
             },
             (ctx, reason) => {
-                this.hooks.emit(
-                    SOCKET_STATUS.interceptorError,
-                    new InterceptorError(reason, ctx.data),
-                );
+                this._emitInterceptorError(reason, ctx.data);
             },
         );
     };
@@ -151,7 +177,7 @@ export class SocketCore {
                     writable: true,
                 },
             },
-        );
+        ) as IInterceptorMiddleware;
     }
 
     private _close = (e: CloseEvent) => {
@@ -162,49 +188,38 @@ export class SocketCore {
         this.hooks.emit(SOCKET_STATUS.error, e);
     };
 
-    public send(params: any) {
-        if (!this._socket) {
-            this.connect();
-        }
-        this._onReady(() => {
-            const context = this._context({...params});
-            this.interceptors.request.start(
-                context,
-                () => {
-                    this._socket?.send(context.data);
-                },
-                (ctx, reason = '') => {
-                    this.hooks.emit(
-                        SOCKET_STATUS.interceptorError,
-                        new InterceptorError(reason, ctx.data),
-                    );
-                },
-            );
-        });
-    }
-
     connect(isRetry?: boolean) {
-        this._catchError(() => {
-            if (this._socket) return;
+        this._catchError(
+            () => {
+                if (this._socket) return;
 
-            const maxRetry = this._config.maxRetry ?? DEFAULT_CONFIG.maxRetry;
+                const maxRetry =
+                    this._config.maxRetry ?? DEFAULT_CONFIG.maxRetry;
 
-            if (isRetry) {
-                if (this._retryCount < maxRetry) {
-                    this._retryCount++;
-                    this.hooks.emit(SOCKET_STATUS.reconnect, this._retryCount);
-                } else {
-                    throw new ReconnectTimeError(
-                        'The number of retry connections has reached the limit，please check your network',
-                    );
+                if (isRetry) {
+                    if (this._retryCount < maxRetry) {
+                        this._retryCount++;
+                        this.hooks.emit(
+                            SOCKET_STATUS.reconnect,
+                            this._retryCount,
+                        );
+                    } else {
+                        throw new ReconnectTimeError(
+                            'The number of retry connections has reached the limit，please check your network',
+                        );
+                    }
                 }
-            }
 
-            this._socket =
-                this._config.adapter?.(this._url) ?? new WebSocket(this._url);
-            this._listen(this._socket);
-            this.hooks.emit(SOCKET_STATUS.connecting);
-        });
+                this._socket =
+                    this._config.adapter?.(this._url) ??
+                    new WebSocket(this._url);
+                this._listen(this._socket);
+                this.hooks.emit(SOCKET_STATUS.connecting);
+            },
+            err => {
+                this.hooks.emit(SOCKET_STATUS.error, err);
+            },
+        );
         return this;
     }
 
@@ -221,11 +236,14 @@ export class SocketCore {
         return this;
     }
 
-    private _catchError(callback: (...args: any[]) => any) {
+    private _catchError(
+        callback: (...args: any[]) => any,
+        catchCallback: (err: Error) => void,
+    ) {
         try {
             callback();
         } catch (error) {
-            this.hooks.emit(SOCKET_STATUS.error, error);
+            catchCallback(error);
         }
     }
 }
