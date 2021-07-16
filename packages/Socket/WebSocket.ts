c08597c33c8d00bee77ee249786172142ef54bc0
debugger007 <yi444978264@gmail.com>
2021-07-16 13:58:47 +0800

    fix: 修复连接问题

--- packages/Socket/WebSocket.ts
+++ packages/Socket/WebSocket.ts
@@ -53,55 +53,72 @@ export class SocketCore {
     static DESTROY_CODE = 1000;
     private _socket: IAdapter | null = null;
     public connected = false;
-    public disconnected = true;
+    private _disposed = false;
     private _config: ISocketCoreConfig;
     private _retryCount = 0;
     private _onReadyHandle: (() => any)[] = [];
     public readonly hooks = new EventEmitter<SOCKET_STATUS>();
+
     public readonly interceptors = {
         request: new Middleware<IInterceptorMiddleware>(),
         response: new Middleware<IInterceptorMiddleware>(),
     };
+
+    public get disconnected() {
+        return !this.connected;
+    }
+
+    public get disposed() {
+        return this._disposed;
+    }
 
     constructor(private _url: string, _config?: ISocketCoreConfig) {
         this._config = {
             ...DEFAULT_CONFIG,
             ..._config,
         };
+        this.registerHooks();
+        this._config.autoConnect && this.connect();
+    }
+
+    public registerHooks() {
         this.hooks.on(SOCKET_STATUS.connect, () => {
             this._retryCount = 0;
             this.connected = true;
-            this.disconnected = !this.connected;
         });
 
         this.hooks.on(SOCKET_STATUS.disconnect, () => {
             this.connected = false;
-            this.disconnected = !this.connected;
-            this._dispose(this._socket);
-            this._socket = null;
+            this._dispose();
             if (this._config.autoReconnect) {
                 setTimeout(() => {
                     this.connect(true);
                 }, this._config.duration || DEFAULT_CONFIG.duration);
             }
         });
-
-        this._config.autoConnect && this.connect();
+        return this;
     }
 
-    private _listen(instance: IAdapter) {
-        instance.addEventListener('open', this._open);
-        instance.addEventListener('message', this._message);
-        instance.addEventListener('close', this._close);
-        instance.addEventListener('error', this._error);
+    private _listen() {
+        if (!this._socket) {
+            this._socket =
+                this._config.adapter?.(this._url) ?? new WebSocket(this._url);
+
+            this.hooks.emit(SOCKET_STATUS.connecting);
+            this._socket.addEventListener('open', this._open);
+            this._socket.addEventListener('message', this._message);
+            this._socket.addEventListener('close', this._close);
+            this._socket.addEventListener('error', this._error);
+        }
     }
 
-    private _dispose(instance: IAdapter | null) {
-        if (instance) {
-            instance.removeEventListener('open', this._open);
-            instance.removeEventListener('message', this._message);
-            instance.removeEventListener('close', this._close);
-            instance.removeEventListener('error', this._error);
+    private _dispose() {
+        if (this._socket) {
+            this._socket.removeEventListener('open', this._open);
+            this._socket.removeEventListener('message', this._message);
+            this._socket.removeEventListener('close', this._close);
+            this._socket.removeEventListener('error', this._error);
+            this._socket = null;
         }
     }
 
@@ -113,11 +130,12 @@ export class SocketCore {
     }
 
     private _open = () => {
+        this.hooks.emit(SOCKET_STATUS.connect);
+
         while (this._onReadyHandle.length) {
             const handle = this._onReadyHandle.shift();
             handle && handle();
         }
-        this.hooks.emit(SOCKET_STATUS.connect);
     };
 
     private _message = (e: MessageEvent) => {
@@ -163,9 +181,6 @@ export class SocketCore {
     };
 
     public send(params: any) {
-        if (!this._socket) {
-            this.connect();
-        }
         this._onReady(() => {
             const context = this._context({...params});
             this.interceptors.request.start(
@@ -184,40 +199,53 @@ export class SocketCore {
     }
 
     connect(isRetry?: boolean) {
-        this._catchError(() => {
-            if (this._socket) return;
+        if (!this._socket) {
+            this._catchError(() => {
+                const maxRetry =
+                    this._config.maxRetry ?? DEFAULT_CONFIG.maxRetry;
 
-            const maxRetry = this._config.maxRetry ?? DEFAULT_CONFIG.maxRetry;
-
-            if (isRetry) {
-                if (this._retryCount < maxRetry) {
-                    this._retryCount++;
-                    this.hooks.emit(SOCKET_STATUS.reconnect, this._retryCount);
-                } else {
-                    throw new ReconnectTimeError(
-                        'The number of retry connections has reached the limit，please check your network',
-                    );
+                if (isRetry) {
+                    if (this._retryCount < maxRetry) {
+                        this._retryCount++;
+                        this.hooks.emit(
+                            SOCKET_STATUS.reconnect,
+                            this._retryCount,
+                        );
+                    } else {
+                        throw new ReconnectTimeError(
+                            'The number of retry connections has reached the limit，please check your network',
+                        );
+                    }
                 }
-            }
 
-            this._socket =
-                this._config.adapter?.(this._url) ?? new WebSocket(this._url);
-            this._listen(this._socket);
-            this.hooks.emit(SOCKET_STATUS.connecting);
-        });
+                this._listen();
+            });
+        }
+
         return this;
     }
 
     public disconnect() {
-        if (this.connected && this._socket) {
-            this._socket.close(SocketCore.DESTROY_CODE, '正常关闭');
+        if (
+            this._socket &&
+            ![ADAPTER_STATUS.CLOSING, ADAPTER_STATUS.CLOSED].includes(
+                this._socket.readyState,
+            )
+        ) {
+            this._socket.close(SocketCore.DESTROY_CODE, 'close');
+        } else {
+            console.warn('Websocket is shutting down or has been shut down');
         }
+
         return this;
     }
 
     public dispose() {
-        this._dispose(this._socket);
-        this.hooks.dispose();
+        if (!this.disposed) {
+            this._dispose();
+            this.hooks.dispose();
+            this._disposed = true;
+        }
         return this;
     }
 
